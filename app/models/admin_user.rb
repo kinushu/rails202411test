4214147527b7c99f82d13c7a1a644eeb64c6ae59
Shuhei Kinukawa <kinushu@gmail.com>
2024-11-01 22:37:51 +0900

    [fix]

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,9 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
+
+  # Ransackで検索可能な属性を指定
+  def self.ransackable_attributes(auth_object = nil)
+    %w[id email created_at updated_at]
+  end
 end
